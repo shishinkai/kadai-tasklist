f23f2105dba8c603d314b760b96757d2b9d44f16
taro.kirameki <goriraitai1128@yahoo.co.jp>
2019-05-01 14:25:05 +0000

    DRY

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_30_103656) do
+ActiveRecord::Schema.define(version: 2019_05_01_124250) do
 
   create_table "tasklists", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_04_30_103656) do
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
